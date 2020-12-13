f5dacb2d7a9405dd95bb5257b582db905a391751
andrey.khoroshko <andrey.khoroshko@forforce.com>
2020-12-13 11:46:37 +0200

    short-url config

--- src/DependencyInjection/UrlShortenerExtension.php
+++ src/DependencyInjection/UrlShortenerExtension.php
@@ -15,9 +15,23 @@ class UrlShortenerExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
+        $config = $this->processConfiguration($configuration, $configs);
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
+        if (isset($config['hash_min_length'])) {
+            $container->setParameter('url_shortener.hash_min_length', $config['hash_min_length']);
+        }
+        if (isset($config['hash_salt'])) {
+            $container->setParameter('url_shortener.hash_salt', $config['hash_salt']);
+        }
+        if (isset($config['hash_alphabet'])) {
+            $container->setParameter('url_shortener.hash_alphabet', $config['hash_alphabet']);
+        }
+        if (isset($config['link_default_ttl'])) {
+            $container->setParameter('url_shortener.link_default_ttl', $config['link_default_ttl']);
+        }
+        if (isset($config['check_user_info'])) {
+            $container->setParameter('url_shortener.check_user_info', $config['check_user_info']);
+        }
     }
 }
